a7151ac294edc116af99502d82c8d197a118feb3
diaoby <183204622@qq.com>
2017-12-05 15:33:09 +0800

    commit

--- src/com/diaoby/pattern/adapter/Adaptee.java
+++ src/com/diaoby/pattern/adapter/Adaptee.java
@@ -2,6 +2,6 @@ package com.diaoby.pattern.adapter;
 
 public class Adaptee{
 	public void doSomething() {
-		System.out.println("=======新实现：我能查110V的插头");
+		System.out.println("=======新实现：我能查110V的插头=============");
 	}
 }
